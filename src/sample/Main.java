8694842fcad52d00c0b641400bdd494d52ce8fa2
qasim bhatti <qasim808@hotmail.co.uk>
2021-11-18 01:51:41 +0500

    Changed structure added UILayer class

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -28,59 +28,17 @@ public class Main extends Application {
         primaryStage.setTitle("Conway Game");
        // StackPane menuPane = new StackPane();
        // SubScene lowerMenu = new SubScene(menuPane, 560, 200);
+        UILayer UIWindow = new UILayer(560, 560, 15);
         VBox root = new VBox();
-        root.getChildren().addAll(getTilePane(560, 560), getMenu());
+        root.getChildren().addAll(UIWindow.getTilePane(), UIWindow.getMenu());
         primaryStage.setResizable(false);
         primaryStage.setScene(new Scene(root, 560, 700));
         primaryStage.show();
 
     }
-    public TilePane getTilePane(double xCord, double yCord){
-        TilePane pane  = new TilePane();
-        int size = 15;
-        pane.setPrefColumns(((int) xCord/size));
-        pane.setPrefRows((int)yCord/size);
-        pane.setTileAlignment(Pos.CENTER);
-        pane.setHgap(1);
-        pane.setVgap(1);
-        for (int i=0; i<((int) xCord/size); i++) {
-            for (int f=0; f<((int) yCord/size - 2); f++) {
-                final Rectangle rect = new Rectangle(size, size, Paint.valueOf("grey"));
-                EventHandler<MouseEvent> eventHand = new EventHandler<MouseEvent>() {
-                    @Override
-                    public void handle(MouseEvent mouseEvent) {
-                        rect.setFill(Paint.valueOf("yellow"));
-                    }
-                };
-                rect.setOnMouseClicked(eventHand);
-                pane.getChildren().add(rect);
-            }
-        }
-        return pane;
-    }
-    public TilePane getMenu(){
-        TilePane pane = new TilePane();
-        //pane.getChildren().addAll(new Separator(Orientation.VERTICAL), new Separator());
-        Button btn = new Button("Start");
-        btn.setPrefSize(70, 50);
-        pane.setAlignment(Pos.BASELINE_CENTER);
-        btn.setStyle("-fx-background-color: #1b2670;");
-        btn.setTextFill(Paint.valueOf("white"));
-        pane.getChildren().add(btn);
-        btn = new Button("Next");
-        btn.setStyle("-fx-background-color: #1b2670;");
-        btn.setTextFill(Paint.valueOf("white"));
-        pane.getChildren().add(btn);
-        btn = new Button("Reset");
-        btn.setStyle("-fx-background-color: #1b2670;");
-        btn.setTextFill(Paint.valueOf("white"));
-        pane.getChildren().add(btn);
-        return pane;
-    }
 
 
     public static void main(String[] args) {
-
         launch(args);
     }
 }
